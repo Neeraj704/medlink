136b91ee94fadfbca6a4c59602b35d7af68a3872
Neeraj704 <neeraj704sky@gmail.com>
2025-09-06 12:18:35 +0530

    Changed backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://medlink2.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
